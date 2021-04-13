d3a906a611ffe365bb7997f18ecf9c8bdf3c32a8
moj-star <Juliusoyebode@yahoo.com>
2021-04-13 04:55:32 -0400

    first commit

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+// Enter your Host, username, password, database below.
+$con = mysqli_connect('localhost', 'root', 'secret', 'muyon');
+// Check connection
+if (mysqli_connect_errno()) {
+    echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+}
